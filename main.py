04a109ac0f1148531bf4ddf669b4e7e5a3e51eae
HLRichardson <101138530+HLRichardson-Git@users.noreply.github.com>
2023-08-25 17:32:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,45 +2,36 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+
+import hashlib
+import hmac
+
 import os
 
-# Simulate Client and Server
-class Client:
-    def __init__(self):
-        self.private_key = rsa.generate_private_key(
-            public_exponent=65537, key_size=2048
-        )
+def prf(secret, label, seed, length):
+    prf_output = b""
+    while len(prf_output) < length:
+        hmac_digest = hmac.new(secret, seed + label, hashlib.sha256).digest()
+        prf_output += hmac_digest
+        seed = hmac_digest
+    
+    return prf_output[:length]
 
-    def send_hello(self):
-        return {
-            "client_hello": {
-                "supported_versions": ["TLSv1.2"],
-                "cipher_suites": ["AES128-SHA256"],
-            }
-        }
+def derive_session_keys(master_secret, client_random, server_random):
+    key_length = 16 # AES-128 key length in bytes
+    iv_length = 16 # AES-128 block size (IV size) in bytes
+    key_block_length = key_length * 2 + iv_length * 2  # Total length of keys and IVs
 
-    def receive_server_hello(self, server_hello):
-        self.server_hello = server_hello
+    # Use the PRF to generate pseudorandom bytes
+    key_block = prf(master_secret, b"key expansion", client_random + server_random, key_block_length)
 
-    def fetch_server_public_key(self):
-        self.server_public_key = serialization.load_pem_public_key(
-            self.server_hello["server_public_key"],
-        )
-        self.shared_secret = os.urandom(16)
+    # Split the key_block into individual keys and IVs
+    client_write_key = key_block[:key_length]
+    server_write_key = key_block[key_length:key_length * 2]
+    client_write_iv = key_block[key_length * 2:key_length * 2 + iv_length]
+    server_write_iv = key_block[key_length * 2 + iv_length:]
 
-    def send_key_exchange(self):
-        encrypted_secret = self.server_public_key.encrypt(
-            self.shared_secret,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None,
-            ),
-        )
-        return {"encrypted_secret": encrypted_secret}
-
-    def receive_session_established(self, session_established):
-        self.session_established = session_established
+    return client_write_key, server_write_key, client_write_iv, server_write_iv
 
 class Server:
     def __init__(self):
@@ -48,25 +39,46 @@ class Server:
             public_exponent=65537, key_size=2048
         )
         self.public_key = self.private_key.public_key()
+        self.server_random_num = os.urandom(32)
+        self.client_random_num = 0
 
-    def send_server_hello(self):
+        self.master_secret = 0
+        self.write_key = 0
+        self.write_IV = 0
+
+        self.client_write_key = 0
+        self.client_write_IV = 0
+
+        self.highest_version = "TLSv1.2"
+        self.selected_cipher_suite = "TLS_RSA_WITH_AES_128_CBC_SHA256"
+    
+    def receive_client_hello(self, client_hello):
+        version = client_hello["client_hello"]["supported_version"]
+        self.client_random_num = client_hello["client_hello"]["randomNum"]
+        cipher_suite = client_hello["client_hello"]["cipher_suite"]
+
+        if version != self.highest_version:
+            print("Unsupported versions, Handshake was unsuccesful")
+            return False
+        if cipher_suite != self.selected_cipher_suite:
+            print("Unsupported cipher suites, Handshake was unsuccesful")
+            return False
+        
+        return version, cipher_suite
+    
+    def send_server_hello(self, highest_version, selected_cipher_suite):
         return {
             "server_hello": {
-                "selected_version": "TLSv1.2",
-                "selected_cipher_suite": "AES128-SHA256",
-                "server_public_key": self.public_key.public_bytes(
-                    encoding=serialization.Encoding.PEM,
-                    format=serialization.PublicFormat.SubjectPublicKeyInfo
-                ),
+                "selected_version": highest_version,
+                "randomNum": self.server_random_num,
+                "sessionID": 0x0000000000000000,
+                "selected_cipher_suite": selected_cipher_suite,
             }
         }
-
-    def receive_client_hello(self, client_hello):
-        pass
-
-    def receive_key_exchange(self, key_exchange):
-        self.shared_secret = self.private_key.decrypt(
-            key_exchange["encrypted_secret"],
+    
+    def recieve_client_key_exchance(self, encrypted_pre_master_secret):
+        self.pre_master_secret = self.private_key.decrypt(
+            encrypted_pre_master_secret,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
                 algorithm=hashes.SHA256(),
@@ -74,24 +86,99 @@ class Server:
             ),
         )
 
-    def send_session_established(self):
-        return {"session_established": True}
 
-# Simulate TLS Handshake
-client = Client()
+
+class Client:
+    def __init__(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537, key_size=2048
+        )
+        self.client_random_num = os.urandom(32) 
+        self.server_random_num = 0
+
+        self.master_secret = 0
+        self.write_key = 0
+        self.write_IV = 0
+
+        self.server_write_key = 0
+        self.server_write_IV = 0
+
+        self.established_version = ""
+        self.established_cipher_suite = ""
+
+    def send_hello(self):
+        return {
+            "client_hello": {
+                "supported_version": "TLSv1.2",
+                "randomNum": self.client_random_num,
+                "sessionID": 0x0000000000000000,
+                "cipher_suite": "TLS_RSA_WITH_AES_128_CBC_SHA256",
+            }
+        }
+    
+    def recieve_server_hello(self, server_hello):
+        self.established_version = server_hello["server_hello"]["selected_version"]
+        self.server_random_num = server_hello["server_hello"]["randomNum"]
+        self.established_cipher_suite = server_hello["server_hello"]["selected_cipher_suite"]
+
+    def client_key_exchange(self, server_public_key):
+        self.pre_master_secret = os.urandom(48)
+        encrypted_pre_master_secret = server_public_key.encrypt(self.pre_master_secret,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None,
+            ),
+        )
+        return encrypted_pre_master_secret
+
 server = Server()
+client = Client()
 
-client_hello = client.send_hello()
-server.receive_client_hello(client_hello)
+client_send_hello = client.send_hello()
+print("1. Client Hello sent succesfully")
 
-server_hello = server.send_server_hello()
-client.receive_server_hello(server_hello)
-client.fetch_server_public_key()  # Fetch the server's public key
+version, cipher_suite = server.receive_client_hello(client_send_hello)
+print("2. Server Hello recieved succesfully")
 
-key_exchange = client.send_key_exchange()
-server.receive_key_exchange(key_exchange)
+server_send_hello = server.send_server_hello(version, cipher_suite)
+print("3. Server Hello sent succesfully")
 
-session_established = server.send_session_established()
-client.receive_session_established(session_established)
+client.recieve_server_hello(server_send_hello)
+print("4. Server Hello recieved succesfully")
 
-print("TLS Handshake completed successfully!")
+server_certificate = server.public_key.public_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo
+                )
+print("5. Server sent certificate succesfully")
+
+encrypted_pre_master_secret = client.client_key_exchange(server.public_key)
+server.recieve_client_key_exchance(encrypted_pre_master_secret)
+print("6. Client sent encrypted pre master secret successfully")
+
+# Both client and server generate the master secret
+client.master_secret = prf(client.pre_master_secret, b"master secret", client.client_random_num + server.server_random_num, 48)
+server.master_secret = prf(server.pre_master_secret, b"master secret", client.client_random_num + server.server_random_num, 48)
+
+if client.master_secret != server.master_secret:
+    print("ERROR: Master secrets do not match")
+print("7. Master secrets succesfully generated")
+
+client_write_key, server_write_key, client_write_iv, server_write_iv = derive_session_keys(client.master_secret, client.client_random_num, server.server_random_num)
+
+# Save the keys into the respective servers
+client.write_key = server.client_write_key = client_write_key
+client.server_write_key = server.write_key = server_write_key
+
+if client.write_key != server.client_write_key and client.server_write_key != server.write_key:
+    print("ERROR: Keys were not generated or stored correctly.")
+print("8.1 Client & Server keys generated and stored correctly")
+
+# Save the Ivs into the respective servers
+client.write_IV = server.client_write_IV = client_write_iv
+client.server_write_IV = server.write_IV = server_write_iv
+
+if client.write_IV != server.client_write_IV and client.server_write_IV != server.write_IV:
+    print("ERROR: IVs were not generated or stored correctly.")
+print("8.2 Client & Server IVs generated and stored correctly")
